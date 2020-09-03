0306b36198f2adfb1d4121660e0eb18877bb7618
MitchDroo <deroom01@gmail.com>
2020-09-03 14:15:51 +1000

    Adds Targets that can be destroyed

--- CSCI356 Game Project/Assets/Scripts/GunController.cs
+++ CSCI356 Game Project/Assets/Scripts/GunController.cs
@@ -14,9 +14,19 @@ public class GunController : MonoBehaviour {
         }
     }
     void Shoot() {
+    	// Initialise raycast hit
     	RaycastHit hit;
+
+    	// If object has been hit by raycast, deal damage
     	if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range)) {
+    		// Display name of hit object
     		Debug.Log(hit.transform.name);
+
+    		// If hit object has target component, deal damage
+    		Target target = hit.transform.GetComponent<Target>();
+    		if (target != null) {
+    			target.TakeDamage(damage);
+    		}
     	}
     }
 }
