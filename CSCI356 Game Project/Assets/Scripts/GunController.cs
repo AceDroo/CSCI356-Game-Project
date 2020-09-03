dbfeb804d986a75270ca932613d80603b604ce5c
MitchDroo <deroom01@gmail.com>
2020-09-03 13:59:38 +1000

    Adds basic hitscan detection

---
+++ CSCI356 Game Project/Assets/Scripts/GunController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+	public float damage = 10f;
+	public float range = 100f;
+
+	public Camera camera;
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+        	Shoot();
+        }
+    }
+    void Shoot() {
+    	RaycastHit hit;
+    	if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range)) {
+    		Debug.Log(hit.transform.name);
+    	}
+    }
+}
