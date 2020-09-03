753267e7541672b6d1e615ab566eac4748d017a7
MitchDroo <deroom01@gmail.com>
2020-09-03 16:37:54 +1000

    Adds Gun Reticle and Laser beam

--- CSCI356 Game Project/Assets/Scripts/GunController.cs
+++ CSCI356 Game Project/Assets/Scripts/GunController.cs
@@ -9,12 +9,20 @@ public class GunController : MonoBehaviour {
 	public float impactForce = 30f;
 
 	public Camera camera;
+    public Transform gunEnd;
 
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
+    private LineRenderer laserLine;
 	private float timeToFire = 0f;
+
+    void Start() {
+        laserLine = GetComponent<LineRenderer>();
+    }
 
     void Update() {
         if (Input.GetButton("Fire1") && Time.time >= timeToFire) {
         	timeToFire = Time.time + 1f  / fireRate;
+            StartCoroutine(ShotEffect());
         	Shoot();
         }
     }
@@ -22,9 +30,16 @@ public class GunController : MonoBehaviour {
     	// Initialise raycast hit
     	RaycastHit hit;
 
+        laserLine.SetPosition(0, gunEnd.position);
+
+        Vector3 rayOrigin = camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+
     	// If object has been hit by raycast, deal damage
     	if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range)) {
-    		// Display name of hit object
+    		// Draw laser line
+            laserLine.SetPosition(1, hit.point);
+
+            // Display name of hit object
     		Debug.Log(hit.transform.name);
 
     		// If hit object has target component, deal damage
@@ -32,6 +47,17 @@ public class GunController : MonoBehaviour {
     		if (target != null) {
     			target.TakeDamage(damage);
     		}
-    	}
+    	} else {
+            laserLine.SetPosition(1, rayOrigin + (camera.transform.forward * range));
+        }
+    }
+    private IEnumerator ShotEffect() {
+        // gunAudio.Play();
+
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
     }
 }
+
+// https://www.youtube.com/watch?v=THnivyG0Mvo <= Brackeys: Shooting with Raycasts - Unity Tutorial
