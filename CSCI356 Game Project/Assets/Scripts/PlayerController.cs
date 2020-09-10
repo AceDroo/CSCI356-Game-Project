ba1b942bed1086a6e33351a7dfcdbfd09d6cf631
harrisonn26 <63281042+harrisonn26@users.noreply.github.com>
2020-09-10 15:53:36 +1000

    Moved physics to FixedUpdate()
    FixedUpdate() is called at a constant rate and it is best practice to perform physics with it

--- CSCI356 Game Project/Assets/Scripts/PlayerController.cs
+++ CSCI356 Game Project/Assets/Scripts/PlayerController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	private float turnSpeed = 4.0f;
-    private float moveSpeed = 2.0f;
+    private float turnSpeed = 4.0f;
+    private float moveSpeed = 4.0f;
 
     private float minTurnAngle = -90.0f;
     private float maxTurnAngle = 90.0f;
     private float rotX;
+    private float rotY;
 
     private float mvX = 0;
     private float mvY = 0;
@@ -24,21 +25,27 @@ public class PlayerController : MonoBehaviour {
     }
     void MouseAiming() {
         // Get mouse inputs
-        float y = Input.GetAxis("Mouse X") * turnSpeed;
+        rotY = Input.GetAxis("Mouse X") * turnSpeed;
         rotX += Input.GetAxis("Mouse Y") * turnSpeed;
 
         // Clamp vertical rotation
         rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
 
-        // Rotate the camera
-        transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0);
+        
+        
     }
     void KeyboardMovement() {
         // Control player movement
         mvX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         mvZ = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
+        
+    }
+    void FixedUpdate() {
+        //Move the player
         transform.Translate(new Vector3(mvX, 0, mvZ));
+        //Rotate the camera
+        transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + rotY, 0);
     }
 }
 
