20d00aaad874f74e21039162d873adeae61127e9
MitchDroo <deroom01@gmail.com>
2020-10-23 12:22:30 +1100

    Updates NetworkManager into WorldManager

---
+++ CSCI356 Game Project/Assets/Scripts/Manager/WorldManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldManager : MonoBehaviour {
+	public GameObject player;
+	public Transform spawnPoint;
+	public GameObject enemySpawner;
+
+	void Start() {
+		GameObject playerObj = Instantiate(player, spawnPoint.position, spawnPoint.rotation);
+
+		enemySpawner.SetActive(true);
+		enemySpawner.GetComponent<EnemySpawner>().target = playerObj;
+	}
+}
