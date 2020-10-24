480593802fbd997a12633d23d23c3fd94e3c09fa
MitchDroo <deroom01@gmail.com>
2020-10-25 10:24:08 +1100

    Migrates Player Spawner to Level

--- CSCI356 Game Project/Assets/Scripts/Manager/StartupText.cs
+++ CSCI356 Game Project/Assets/Scripts/Manager/StartupText.cs
@@ -14,6 +14,7 @@ public class StartupText : MonoBehaviour {
 	int remainSeconds;
 
 	void Start() {
+		player = FindObjectOfType<PlayerController>();
 		audioManager.Play("Prepare");
 
 		remainSeconds = 10;
